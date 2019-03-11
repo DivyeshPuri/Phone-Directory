f9d097fa87f97751a22654fb7b3d0275efe07a77
Divyesh Puri <divyeshpuri66@gmail.com>
2019-03-12 03:36:02 +0530

    Fixed naming

---
+++ build/precache-manifest.416cd5b7ed51d9c9033ec99fd352ce72.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5015168ad240764bbad6",
+    "url": "/static/css/main.724e2647.chunk.css"
+  },
+  {
+    "revision": "5015168ad240764bbad6",
+    "url": "/static/js/main.7284cf33.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "eeeaef47cfa85bd567cd",
+    "url": "/static/js/2.39bffbd2.chunk.js"
+  },
+  {
+    "revision": "a601e508cc68ec0fd11258723e9621da",
+    "url": "/index.html"
+  }
+];
